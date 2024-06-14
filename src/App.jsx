b61a1b0379506360ac3c561f95e3c9491d748202
Tanguy D. <tanguy.davidpro85@gmail.com>
2024-06-14 20:35:52 +0200

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Parcours from "./components/Parcours";
 import Main from "./components/Main";
